b7fd8bb28464de40f2dba22fd40099ec1c75a651
Vincent <72398145+wcvincentlam@users.noreply.github.com>
2022-07-24 21:29:51 +0800

    Updated nav bar logic for mobile devices

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let navToggle = document.querySelector('.nav__list--m');
 let navLinks = document.querySelectorAll('.nav__link');
+let sectionToggle = document.querySelector('.content');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
+/* removes navigation bar when link is clicked for mobile */
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
 });
+
+/* removes navigation bar when section contents are clicked - for mobile */
+sectionToggle.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+});
+
+
