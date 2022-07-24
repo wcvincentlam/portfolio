408346e57e8af61299b9177fced945b65aceb23a
Vincent <72398145+wingcv@users.noreply.github.com>
2022-07-24 12:14:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let navToggle = document.querySelector('.nav__list--m');
+let navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+});
